eddbe1eb04e43705b3ca3345ef95326ee1d534be
Jeremy Southon <southon55@gmail.com>
2018-10-10 23:00:49 +1300

    Added moveable pins, however pins that others place cannot be moved

--- index.js
+++ index.js
@@ -9,42 +9,39 @@ function initMap() {
     );
     console.log("Loaded map");
 
+
     //loading previously added spottings
     loadSpottings(map);
+
+    //add new markers
     map.addListener('click', function (e) {
         if (placingSpotting) {
             placeMarkerAndPanTo(e.latLng, map);
         }
     })
 
-    // var message = prompt("Care to leave a message :3");//TODO add to firebase
+    
 }
 
-loadSpottings = async(map) => {
-    const fireBaseSpottings = await fetch('https://gitsearch-ff7bc.firebaseio.com/spottings.json?print=pretty').then(res => res.json())
-    const keys = await Object.keys(fireBaseSpottings);
-    const spottings = await keys.map(key => ({
-        lat: fireBaseSpottings[key].lat,
-        lng: fireBaseSpottings[key].lng
-    }));
-    //now load into map
-    await spottings.forEach((e)=> {
-       console.log(e);
-       new google.maps.Marker({
-            position: e,
-            map: map
-        });
-    })
+leaveMessage = async()=>{
+    var message = prompt("Care to leave a message :3");//TODO add to firebase
+    const response = await fetch("https://gitsearch-ff7bc.firebaseio.com/messages.json", {
+        method: "POST",
+        body: JSON.stringify(message)
+    }).catch(err => console.log(err));
 }
 
 function placeMarkerAndPanTo(latLng, map) {
     addMarkerToCollection(latLng);
     var marker = new google.maps.Marker({
         position: latLng,
-        map: map
+        map: map,
+        draggable: true
     });
     map.panTo(latLng);
+    
 }
+
 
 
 //FIREBASE STUFF
@@ -56,4 +53,20 @@ addMarkerToCollection = async (latLng) => {
         body: JSON.stringify(latLng)
     }).catch(err => console.log(err));
     console.log("added")
+}
+
+loadSpottings = async(map) => {
+    const fireBaseSpottings = await fetch('https://gitsearch-ff7bc.firebaseio.com/spottings.json?print=pretty').then(res => res.json())
+    const keys = await Object.keys(fireBaseSpottings);
+    const spottings = await keys.map(key => ({
+        lat: fireBaseSpottings[key].lat,
+        lng: fireBaseSpottings[key].lng
+    }));
+    //now load into map
+    await spottings.forEach((e)=> {
+       new google.maps.Marker({
+            position: e,
+            map: map,
+        });
+    })
 }
